c62db9fc3aa8874138c627daf69c8fae7bc48cf4
Aram Mine <aram.mine@gaiax.com>
2019-11-15 01:31:11 +0900

    ページの追加

---
+++ pages/timeago.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="box" data-title="Output" v-if="date">
+            <timeago :datetime="date" :auto-update="60"/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            date: new Date()
+        }
+    }
+}
+</script>
